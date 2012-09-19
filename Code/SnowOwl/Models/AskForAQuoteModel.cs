643c72a9bdf4005b70858f95b100708a07af71ef
Alexander <web.kpaba@gmail.com>
2012-09-19 16:50:33 +0300

    Ask for a quote form
    Partial View for "Ask for a quote form" and dedicated Model class

---
+++ Code/SnowOwl/Models/AskForAQuoteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SnowOwl.Models
+{
+    public class AskForAQuoteModel
+    {
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "Not valid email")]
+        public String Email { get; set; }
+
+        [Required]
+        public String Message { get; set; }
+    }
+}
