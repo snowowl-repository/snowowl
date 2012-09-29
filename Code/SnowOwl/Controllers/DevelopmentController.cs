b61343c37fde3351d772c7da2540f8e55f4a1aa3
Alexander Krsvchuk <web.kpaba@gmail.com>
2012-09-29 11:03:57 +0300

    contact us

--- Code/SnowOwl/Controllers/DevelopmentController.cs
+++ Code/SnowOwl/Controllers/DevelopmentController.cs
@@ -4,6 +4,11 @@ namespace SnowOwl.Controllers
 {
     public class DevelopmentController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
         public ActionResult Software()
         {
             return View();
