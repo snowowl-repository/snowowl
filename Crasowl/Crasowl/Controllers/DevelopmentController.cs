29d0cfe2a88103ea0def5216286355ec9dafb997
Alexander <web.kpaba@gmail.com>
2012-10-01 12:11:49 +0300

    Layout and css styles

---
+++ Crasowl/Crasowl/Controllers/DevelopmentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Crasowl.Controllers
+{
+    public class DevelopmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Software()
+        {
+            return View();
+        }
+
+        public ActionResult Web()
+        {
+            return View();
+        }
+
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+    }
+}
